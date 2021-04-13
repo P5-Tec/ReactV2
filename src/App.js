27fade4e12758f755609168724413dbb0e9c6dc0
DainisM <moisejenko11@gmail.com>
2021-04-13 10:50:21 +0200

    Added intl (multilanguage) package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
+import { IntlProvider } from "react-intl";
+import { flattenMessages } from "./utils";
 import "./styles/App.css";
+
+import messages from "./messages";
+
+import "@formatjs/intl-relativetimeformat/polyfill";
+import "@formatjs/intl-relativetimeformat/locale-data/en";
+import "@formatjs/intl-relativetimeformat/locale-data/da";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 
+let locale = (navigator.languages && navigator.languages[0]) ||
+	navigator.language ||
+	navigator.userLanguage || ["en-US", "en"];
+
 function App() {
 	const location = useLocation();
+	const [language, setLanguage] = useState(locale);
 
 	return (
-		<>
-			<Header />
-			<Switch location={location} key={location.key}>
-				<Route path="/" component={Home} exact />
+		<IntlProvider
+			locale={locale}
+			messages={flattenMessages(messages[language])}
+		>
+			<>
+				<Header language={language} setLanguage={setLanguage} />
+				<Switch location={location} key={location.key}>
+					<Route path="/" component={Home} exact />
 
-				<Route path="/contact" component={Contact} />
-			</Switch>
-			<Footer />
-		</>
+					<Route path="/contact" component={Contact} />
+				</Switch>
+				<Footer />
+			</>
+		</IntlProvider>
 	);
 }
 
